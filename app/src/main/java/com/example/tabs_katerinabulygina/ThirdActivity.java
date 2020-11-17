80936bcf5524696dfcd2cab4790555b8ff4ce0db
Bulygin Juriy <bu@sdi-studio.ru>
2020-11-17 19:26:50 +0300

    Практическая работа7.Приложения с вкладками

---
+++ app/src/main/java/com/example/tabs_katerinabulygina/ThirdActivity.java
@@ -0,0 +1,22 @@
+package com.example.tabs_katerinabulygina;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ThirdActivity extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(@Nullable LayoutInflater inflater, @NonNull ViewGroup container,
+                             @Nullable Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.third_tab, container, false);
+    }
+
+}
